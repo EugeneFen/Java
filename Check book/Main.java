4d8bb2f2aac4e63deeb731fbd85bd32ab2fc385f
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-10-30 06:25:03 +0300

    Create Main.java

---
+++ Check book/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println(book_data_check.IBM("1347593849562"));
+        //System.out.println(book_data_check.IBM("13475938495"));
+        //System.out.println(book_data_check.IBM("134759384562"));
+        //System.out.println(book_data_check.IBM("13475934849562"));
+        //System.out.println(book_data_check.IBM("13475r3849562"));
+
+
+
+        //System.out.println();
+
+        //System.out.println(book_data_check.Name("Мы"));
+        //System.out.println(book_data_check.Name("Там где синеют маки"));
+        //System.out.println(book_data_check.Name("1965"));
+        //System.out.println(book_data_check.Name("Я"));
+        //System.out.println(book_data_check.Name("Я/"));
+        //System.out.println(book_data_check.Name("Я 45*"));
+        //System.out.println();
+
+        //System.out.println(book_data_check.Publishing_house("1"));
+        //System.out.println(book_data_check.Publishing_house("ghjfd"));
+        //System.out.println(book_data_check.Publishing_house("Gdnjn"));
+        //System.out.println(book_data_check.Publishing_house("Fjnv "));
+        //System.out.println(book_data_check.Publishing_house(" "));
+        //System.out.println();
+
+        //System.out.println(book_data_check.Place("1"));
+        //System.out.println(book_data_check.Place("Казахстан"));
+        //System.out.println(book_data_check.Place("Казахстан "));
+        //System.out.println(book_data_check.Place("Нов-город"));
+        //System.out.println(book_data_check.Place("Хабар овск"));
+        //System.out.println(book_data_check.Place(" "));
+        //System.out.println();
+
+        //System.out.println(book_data_check.Number_of_pages("1"));
+        //System.out.println(book_data_check.Number_of_pages("13"));
+        //System.out.println(book_data_check.Number_of_pages("135"));
+        //System.out.println(book_data_check.Number_of_pages("1345"));
+        //System.out.println(book_data_check.Number_of_pages(""));
+        //System.out.println();
+
+        //System.out.println(book_data_check.Year("1"));
+        //System.out.println(book_data_check.Year("12"));
+        //System.out.println(book_data_check.Year("134"));
+        //System.out.println(book_data_check.Year("1356"));
+        //System.out.println(book_data_check.Year("1356 "));
+        //System.out.println(book_data_check.Year("135d"));
+        //System.out.println(book_data_check.Year("13756"));
+        //System.out.println();
+
+        //System.out.println(book_data_check.Author("Иванов Иван Иванович"));
+        //System.out.println(book_data_check.Author("Ив3нов Иван Иванович"));
+        //System.out.println(book_data_check.Author("Иванов  Иван Иванович"));
+        //System.out.println(book_data_check.Author("иванов Иван Иванович"));
+        //System.out.println(book_data_check.Author("Ив Иван Иванович"));
+        //System.out.println(book_data_check.Author("Иванов5 Иван Иванович"));
+        //System.out.println();
+
+    }
+}
