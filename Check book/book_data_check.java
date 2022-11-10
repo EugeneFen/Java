3922e566d9d07f262cb065e65572046bfa60c081
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-11-10 13:27:10 +1000

    Update book_data_check.java

--- Check book/book_data_check.java
+++ Check book/book_data_check.java
@@ -2,9 +2,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class book_data_check {
-    public static boolean Name(String name)
+    public static boolean Autor(String name)
     {
-        Pattern pattern = Pattern.compile("[\sA-Za-zа-яА-Я0-9]*");
+        Pattern pattern = Pattern.compile("(\\s*[A-ZА-Я][a-zа-я]{1,20}\\s[A-ZА-Я][a-zа-я]{1,20}\\s[A-ZА-Я][a-zа-я]{1,20}\\s*){1,5}[#]");
         Matcher matcher = pattern.matcher(name);
         return matcher.matches();
     }
@@ -15,7 +15,7 @@ public class book_data_check {
         Одно слово с заглавной буквы без пробелов.
         "Эсмо" "Карл"
          */
-        Pattern pattern = Pattern.compile("[A-ZА-Я]([a-zA-Zа-яА-Я]*)");
+        Pattern pattern = Pattern.compile("\\s*[A-ZА-Я][a-zа-я]{3,25}\\s*[#]");
         Matcher matcher = pattern.matcher(name);
         return matcher.matches();
     }
@@ -25,7 +25,7 @@ public class book_data_check {
         /*
         Название города. Слово, два слова или название через дефиз. После слова может быть пробел
          */
-        Pattern pattern = Pattern.compile("[A-ZА-Я]([a-zA-Zа-яА-Я]*)([-]*)([\sa-zA-Zа-яА-Я]*)");
+        Pattern pattern = Pattern.compile("\\s*[A-ZА-Я][a-zа-я]{2,25}([-][A-ZА-Я][a-zа-я]{2,25})?(\\s[A-ZА-Я]?[a-zа-я]{2,25}){0,2}\\s*[#]");
         Matcher matcher = pattern.matcher(name);
         return matcher.matches();
     }
@@ -35,7 +35,7 @@ public class book_data_check {
         /*
 
         */
-        Pattern pattern = Pattern.compile("[0-9]{13}");
+        Pattern pattern = Pattern.compile("\\s*97[89]5([0-9]{9})\\s*[#]");
         Matcher matcher = pattern.matcher(num);
         return matcher.matches();
     }
@@ -46,12 +46,12 @@ public class book_data_check {
         Формат "1789"
         Только 4 цифры. Никаких пробелов.
         */
-        Pattern pattern = Pattern.compile("[0-9]{4}");
+        Pattern pattern = Pattern.compile("\\s*[12][098][0-9]{2}\\s*[#]");
         Matcher matcher = pattern.matcher(year);
         return matcher.matches();
     }
 
-    public static boolean Author(String name)
+    public static boolean Name(String name)
     {
         /*
         Фармат "Иванов Иван Иванович"
@@ -59,7 +59,7 @@ public class book_data_check {
         каждое слово пишется с заглавной буквы
         перед словом и после пробедов быть не должно
          */
-        Pattern pattern = Pattern.compile("[A-ZА-Я]([a-zA-Zа-яА-Я]{3})([a-zA-Zа-яА-Я]*)\\s([A-ZА-Я]([a-zA-Zа-яА-Я]{3})([a-zA-Zа-яА-Я]*))\\s([A-ZА-Я]([a-zA-Zа-яА-Я]{3})([a-zA-Zа-яА-Я]*))");
+        Pattern pattern = Pattern.compile("\\s*([A-ZА-Я][a-zа-я]{0,15}(\\s[A-ZА-Я]?[a-zа-я]{0,16}){0,10})?([0-9]{1,4})?\\s*[#]");
         Matcher matcher = pattern.matcher(name);
         return matcher.matches();
     }
@@ -70,7 +70,7 @@ public class book_data_check {
         Формат "1" "13" "125"
         Страниц до 1000.
          */
-        Pattern pattern = Pattern.compile("[0-9]{1,3}");
+        Pattern pattern = Pattern.compile("\\s*[0-9]{1,3}\\s*[#]");
         Matcher matcher = pattern.matcher(number);
         return matcher.matches();
     }
