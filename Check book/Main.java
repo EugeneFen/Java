fac2c456db784a30b498b47f46bc9ae6ccd2a862
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-11-10 13:26:37 +1000

    Update Main.java

--- Check book/Main.java
+++ Check book/Main.java
@@ -1,61 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+    public static boolean Name(String name)
+    {
+        Pattern pattern = Pattern.compile("(\\s*[A-ZА-Я][a-zа-я]{1,20}\\s[A-ZА-Я][a-zа-я]{1,20}\\s[A-ZА-Я][a-zа-я]{1,20}\\s*){1,5}[#]\\s*([A-ZА-Я][a-zа-я]{0,15}(\\s[A-ZА-Я]?[a-zа-я]{0,16}){0,10})?([0-9]{1,4})?\\s*[#]\\s*[A-ZА-Я][a-zа-я]{3,25}\\s*[#]\\s*[A-ZА-Я][a-zа-я]{2,25}([-][A-ZА-Я][a-zа-я]{2,25})?(\\s[A-ZА-Я]?[a-zа-я]{2,25}){0,2}\\s*[#]\\s*[12][098][0-9]{2}\\s*[#]\\s*[0-9]{1,3}\\s*[#]\\s*97[89]5([0-9]{9})\\s*");
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+
     public static void main(String[] args) {
-        //System.out.println(book_data_check.IBM("1347593849562"));
-        //System.out.println(book_data_check.IBM("13475938495"));
-        //System.out.println(book_data_check.IBM("134759384562"));
-        //System.out.println(book_data_check.IBM("13475934849562"));
-        //System.out.println(book_data_check.IBM("13475r3849562"));
 
-
-
-        //System.out.println();
-
-        //System.out.println(book_data_check.Name("Мы"));
-        //System.out.println(book_data_check.Name("Там где синеют маки"));
-        //System.out.println(book_data_check.Name("1965"));
-        //System.out.println(book_data_check.Name("Я"));
-        //System.out.println(book_data_check.Name("Я/"));
-        //System.out.println(book_data_check.Name("Я 45*"));
-        //System.out.println();
-
-        //System.out.println(book_data_check.Publishing_house("1"));
-        //System.out.println(book_data_check.Publishing_house("ghjfd"));
-        //System.out.println(book_data_check.Publishing_house("Gdnjn"));
-        //System.out.println(book_data_check.Publishing_house("Fjnv "));
-        //System.out.println(book_data_check.Publishing_house(" "));
-        //System.out.println();
-
-        //System.out.println(book_data_check.Place("1"));
-        //System.out.println(book_data_check.Place("Казахстан"));
-        //System.out.println(book_data_check.Place("Казахстан "));
-        //System.out.println(book_data_check.Place("Нов-город"));
-        //System.out.println(book_data_check.Place("Хабар овск"));
-        //System.out.println(book_data_check.Place(" "));
-        //System.out.println();
-
-        //System.out.println(book_data_check.Number_of_pages("1"));
-        //System.out.println(book_data_check.Number_of_pages("13"));
-        //System.out.println(book_data_check.Number_of_pages("135"));
-        //System.out.println(book_data_check.Number_of_pages("1345"));
-        //System.out.println(book_data_check.Number_of_pages(""));
-        //System.out.println();
-
-        //System.out.println(book_data_check.Year("1"));
-        //System.out.println(book_data_check.Year("12"));
-        //System.out.println(book_data_check.Year("134"));
-        //System.out.println(book_data_check.Year("1356"));
-        //System.out.println(book_data_check.Year("1356 "));
-        //System.out.println(book_data_check.Year("135d"));
-        //System.out.println(book_data_check.Year("13756"));
-        //System.out.println();
-
-        //System.out.println(book_data_check.Author("Иванов Иван Иванович"));
-        //System.out.println(book_data_check.Author("Ив3нов Иван Иванович"));
-        //System.out.println(book_data_check.Author("Иванов  Иван Иванович"));
-        //System.out.println(book_data_check.Author("иванов Иван Иванович"));
-        //System.out.println(book_data_check.Author("Ив Иван Иванович"));
-        //System.out.println(book_data_check.Author("Иванов5 Иван Иванович"));
-        //System.out.println();
-
+        System.out.println(Name("Иванов Иван Иванович Rad Kail Martin#We are# Screan # Moscow-Siti#"));
+		System.out.println(Name("Иванов Иван Иванович4 # We are are you # Series # Moscow# 1956 # 145 # 1459346274839"));
+		System.out.println(Name("Иванов Иван Иванович # We are are you3 # Series # Moscow# 1956 # 145 # 1459346274839"));
+		System.out.println(Name("Иванов Иван Иванович # We are are you # Series ha # Moscow# 1956 # 145 # 1459346274839"));
+		System.out.println(Name("Иванов Иван Иванович # We are are you # Series # Moscow and Vlad# 1956 # 145 # 1459346274839"));
+		System.out.println(Name("Иванов Иван Иванович # We are are you # Series # Moscow# 19564 # 145 # 1459346274839"));
+		System.out.println(Name("Иванов Иван Иванович # We are are you # Series # Moscow# 1956 # 1453 # 1459346274839"));
+        System.out.println(Name("Иванов Иван Иванович # We are are you # Series # Moscow# 1956 # 145 # 9795346274839"));
+		System.out.println(Name(" Семченко Иванна Олеговна  #  We are you # Russia # Russia in Franse # 1956 # 145 # 9785450789045"));
+    
+    
+   // book_data_check a = new book_data_check();
+    //System.out.println(book_data_check.Autor("  Семченко Иванна Олеговна   #"));
+    //System.out.println(book_data_check.Name(" We are you #"));
+    //System.out.println(book_data_check.Publishing_house(" Russia #"));
+    //System.out.println(book_data_check.Place(" Russia #"));
+    //System.out.println(book_data_check.Place(" Russia-Moscow #"));
+    //System.out.println(book_data_check.Place(" Russia in Franse #"));
+   // System.out.println(book_data_check.Year(" 1955 #"));
+    //System.out.println(book_data_check.IBM(" 9785450789045 #"));
+    //System.out.println(book_data_check.Number_of_pages(" 1 #"));
     }
 }
