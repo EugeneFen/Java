00d02a9d257f2485b629777275284309908f39a6
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-11-15 15:47:34 +0300

    Create MasException.java

---
+++ Mass/MasException.java
@@ -0,0 +1,10 @@
+public class MasException extends Exception {
+    public String toString1()
+    {
+        return ("Out of bounds array < 5");
+    }
+    public String toString2()
+    {
+        return ("Out of bounds array > 5");
+    }
+}
