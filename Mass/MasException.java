f3acddb204b1a1744bcc18b5f94a3f9adeb8542f
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-11-24 06:12:16 +0300

    Update MasException.java

--- Mass/MasException.java
+++ Mass/MasException.java
@@ -1,13 +1,14 @@
+//Класс с сообщениями об ошибке
 public class MasException extends Exception {
-    public String toString1()
+    public String toString1() //при переполнении добавление
     {
         return ("Out of bounds array < 5");
     }
-    public String toString2()
+    public String toString2() //при пустом массиве удаление
     {
         return ("Out of bounds array > 5");
     }
-    public String toString3()
+    public String toString3() //при поиске в пустом массиве
     {
         return ("Array is empty");
     }
