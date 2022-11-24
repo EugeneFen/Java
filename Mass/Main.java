601e4feccf53c6bdf3cca8fbdd633b28846f7b66
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2022-11-24 06:06:33 +0300

    Update Main.java

--- Mass/Main.java
+++ Mass/Main.java
@@ -2,15 +2,16 @@ public class Main {
     public static void main(String[] args) throws MasException {
         Excepti a = new Excepti();
 
-            //for(int i = 0; i<7; i++) a.setAdd(i);
-          //  Excepti.setAdd(1); a.setAdd(2); a.setAdd(3); a.setAdd(4); a.setAdd(5); a.setAdd(6);
-       // a.setAdd(6); a.setAdd(6);
-         //System.out.println(a.getElement(0));
-        a.DelElement();
+        //for(int i = 0; i<7; i++) a.setAdd(i);
+        //  Excepti.setAdd(1); a.setAdd(2); a.setAdd(3); a.setAdd(4); a.setAdd(5); a.setAdd(6);
+        // a.setAdd(6); a.setAdd(6);
+        //System.out.println(a.getElement(0));
+        //a.DelElement(); a.DelElement();
        /* for (int i = 0; i<5; i++)
         {
             System.out.println(a.getElement(i));
         }*/
+        a.Search(8);
 
     }
 }
